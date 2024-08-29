8099cb69a6cda40eb8df734708d0c4582d90dca8
ErickNurPraditya <ericknp783@gmail.com>
2024-08-29 13:35:01 +0700

    push kedua

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
+
 function ConditionalRendering() {
   const isLoggedIn = true;
 
   return (
+    
     <div>
       {isLoggedIn ? (
         <h1>Welcome back!</h1>
